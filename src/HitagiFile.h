a2ffd0701b7cd31fc09456a3efd64161cf023f76
Ben <ben@july7.pw>
2018-03-05 11:21:43 -0500

    add some documentation for hitagi files

--- src/HitagiFile.h
+++ src/HitagiFile.h
@@ -4,6 +4,19 @@
 #include <string>
 
 namespace Engine{
+    /*
+      HitagiFile
+      This class handles the loading of the scene files.
+      Specifically, it does not handle the parsing, it simply 
+      contains the raw data and some helper functions to process
+      the data inside of the class.
+
+      HitagiFiles exist as a blob file containing all the scripts
+      and bitmaps of the game. It starts with a header that simply
+      reads hitagi-engine followed by 0x0A indicating a newline.
+      From there headers with the name of the file followed by
+      <-EOF-> separate the files.
+     */
     class HitagiFile{
         
         // the container of the file
