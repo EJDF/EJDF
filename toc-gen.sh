d2995ee1871bc2322d2fd840655b16e3ffdc5295
Ben <ben@july7.pw>
2018-03-06 10:00:57 -0500

    update the toc format because the parenthesis didn't make sense

--- toc-gen.sh
+++ toc-gen.sh
@@ -3,15 +3,15 @@
 
 # Generally, the table of contents work by assigning a symbol to a byte offset
 # each asset is 10 bytes apart and is given a size
-# NAME=(START,STOP)
-# testbmp=(500,3694)
+# NAME=START,STOP
+# testbmp=500,3694
 
 byte=100
 
 for filename in ./*; do
     # get filesize
     size=`wc -c < $filename`
-    echo "`basename ${filename//.}`=($byte,$(($byte + size)))"
+    echo "`basename ${filename//.}`=$byte,$(($byte + size))"
     # create the offset of size + 10 (10 byte buffer)
     byte=$(($byte + size + 10))
 done
